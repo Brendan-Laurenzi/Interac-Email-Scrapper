8831d87cfd6ce815ac5d04977b0ff10978cfe832
Brendan-Laurenzi <119980970+Brendan-Laurenzi@users.noreply.github.com>
2023-11-07 11:10:24 -0500

    Started GUI Development
    - Added Basic GUI functionality as main.c
    - Updated GUI sample design

--- main.py
+++ main.py
@@ -1,46 +1,56 @@
-import imaplib
-import email
-from email.header import decode_header
+import tkinter
+import tkinter.messagebox
+import customtkinter
+from CTkTable import *
 
-# Your email and password
-email_user = "prolittle101@hotmail.com"
-email_pass = "8wLdPV_PL5$G5bj"
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-# Connect to the IMAP server (Gmail in this example)
-mail = imaplib.IMAP4_SSL("outlook.office365.com")
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-# Log in to your email account
-mail.login(email_user, email_pass)
+        self.title("EMT MANAGER")
+        self.geometry(f"{1100}x{580}")
 
-# Select the mailbox you want to work with (e.g., "INBOX" for your inbox)
-mail.select("INBOX")
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((0,2), weight=0)
+        self.grid_rowconfigure((1), weight=1)
 
-# Search for the email you want (e.g., by subject)
-email_subject = "INTERAC"
-result, data = mail.search(None, f'SUBJECT "{email_subject}"')
+        # Side Bar
 
-# Get the list of email IDs that match the search criteria
-email_id_list = data[0].split()
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=2, sticky="nsew")
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="EMT MANAGER", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        self.report_button = customtkinter.CTkButton(self.sidebar_frame, text="Report", command=self.sidebar_button_event)
+        self.report_button.grid(row=1, column=0, padx=20, pady=10)
+        self.settings_button = customtkinter.CTkButton(self.sidebar_frame, text="Settings", command=self.sidebar_button_event)
+        self.settings_button.grid(row=2, column=0, padx=20, pady=10)
 
-# Assuming you want to fetch the first email that matches the criteria
-for email_id in email_id_list:
-    result, message_data = mail.fetch(email_id, "(RFC822)")
+        # Search Bar
 
-    # Parse the email content
-    raw_email = message_data[0][1]
-    email_message = email.message_from_bytes(raw_email)
+        self.search_frame = customtkinter.CTkFrame(self, height=35, corner_radius=0)
+        self.search_frame.grid(row=0, column=1, rowspan=1, padx=10, pady=(10,0), sticky="nsew")
+        self.search_entry = customtkinter.CTkEntry(self, placeholder_text="Description")
+        self.search_entry.grid(row=0, column=1, padx=10, pady=(10,0), sticky="nsew")
+        self.search_button = customtkinter.CTkButton(master=self, text="Search", fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
+        self.search_button.grid(row=0, column=2, padx=(0,10), pady=(10,0), sticky="nsew")
 
-    # Extract the email text
-    email_text = ""
+        # Table Data
 
-    for part in email_message.walk():
-        if part.get_content_type() == "text/plain":
-            email_text = part.get_payload(decode=True).decode(part.get_content_charset(), errors="ignore")
+        self.data_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.data_frame.grid(row=1, column=1, columnspan=2, padx=10, pady=10, sticky="nsew")
 
-    print("Email text:")
-    print(email_text)
-    print("="*100)
+        self.emt_table = CTkTable(master=self.data_frame, corner_radius=0, row=5, column=5, values=[["Name","Amount","Memo","Date","Ref_ID"]])
+        self.emt_table.grid(row=2, column=1, columnspan=2, sticky="nsew")
 
-# Close the mailbox and log out
-mail.close
-mail.logout()
+    def sidebar_button_event(self):
+        print("sidebar_button click")
+
+    def search_button_event(self):
+        print("sidebar_button click")
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
